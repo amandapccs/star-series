e28b92066c91934eeed5a70a351de716208ec29d
Bernardo Salgueiro <bernardo.roll@gmail.com>
2022-03-30 08:28:37 -0300

    Adiciona arquivos iniciais

---
+++ helpers/fetchSeries.js
@@ -0,0 +1,10 @@
+const fetchSeries = async () => {
+  const url = 'https://api.tvmaze.com/shows';
+  // Seu código aqui
+};
+
+if (typeof module !== 'undefined') {
+  module.exports = {
+    fetchSeries,
+  };
+}
